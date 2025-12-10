11113c72a5d6dd085d10aa5ae0df8b31cb9934d1
yuchen <candy301.wu@gmail.com>
2025-12-10 13:07:23 +0800

    modified: level.h

--- Level.h
+++ Level.h
@@ -9,16 +9,16 @@ class Level {
 public:
     Level() { init(); }
 
-    void init();                 // 初始化
-    void load_level(int lvl);    // 載入關卡（生成怪物）
-    void update();               // 更新怪物或其他需要更新的東西
-    void draw();                 // 畫出背景圖片（如果有）
+    void init();                 
+    void load_level(int lvl);   
+    void update();             
+    void draw();              
 
     std::string get_background() const { return background_path; }
 
 private:
     int level;
-    std::string background_path; // 背景圖片路徑
+    std::string background_path; 
 };
 
 #endif
